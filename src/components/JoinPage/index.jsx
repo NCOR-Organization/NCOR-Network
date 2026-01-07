7032ede90062591a8a95e5a3fe0bffbf48a3fc25
tinazunner <tinazunner@gmail.com>
2026-01-07 12:24:04 -0500

    Update index.jsx

--- src/components/JoinPage/index.jsx
+++ src/components/JoinPage/index.jsx
@@ -4,6 +4,7 @@ import JoinHero from './JoinHero';
 import BenefitsSection from './BenefitsSection';
 import ApplicationSection from './ApplicationSection';
 import ContactSection from './ContactSection';
+import styles from './JoinPage.module.css';
 
 const JoinPage = () => {
     const [initialMembershipType, setInitialMembershipType] = useState(null);
@@ -24,53 +25,53 @@ const JoinPage = () => {
       <JoinHero />
       
       <div className="container margin-top--xl margin-bottom--xl">
-        <div className="row">
-          <div className="col col--10 col--offset-1">
-            {/* Membership options at the top */}
-<div style={{ marginBottom: '2rem' }}>
-  <h2>Membership Options</h2>
-  <p>
-    Choose the membership type that best fits you. GitHub is not required to join NCOR.
-  </p>
-
   <div className="row">
-    <div className="col col--6">
-      <div className="card">
-        <div className="card__body">
-          <h3>Individual Membership</h3>
-          <p>
-            Join as an individual researcher or practitioner. GitHub is optional and
-            only needed for participation in certain technical working groups.
-          </p>
-          <button
-            className="button button--primary"
-            type="button"
-            onClick={() => goToApplication('individual')}
-          >
-            Apply as an Individual
-          </button>
-        </div>
-      </div>
-    </div>
+    <div className="col col--10 col--offset-1">
+      {/* Membership options at the top */}
+      <div style={{ marginBottom: '2rem' }}>
+        <h2>Membership Options</h2>
+        <p>
+          Choose the membership type that best fits you.
+        </p>
 
-    <div className="col col--6">
-      <div className="card">
-        <div className="card__body">
-          <h3>Organizational Membership</h3>
-          <p>
-            Join as a lab, company, institute, or project. Organizational membership
-            does not require GitHub.
-          </p>
-          <button
-            className="button button--secondary"
-            type="button"
-            onClick={() => goToApplication('organization')}
-          >
-            Apply as an Organization
-          </button>
-        </div>
-      </div>
-    </div>
+        <div className="row">
+          <div className="col col--6">
+            <div className="card">
+              <div className="card__body">
+                <h3>Individual Membership</h3>
+                <p>
+                  Join as an individual researcher or practitioner. GitHub is optional and
+                  only needed for participation in certain technical working groups.
+                </p>
+                <button
+                  className={`button ${styles.ncorButton}`}
+                  type="button"
+                  onClick={() => goToApplication('individual')}
+                >
+                  Apply as an Individual
+                </button>
+              </div>
+            </div>
+          </div>
+
+            <div className="col col--6">
+            <div className="card">
+              <div className="card__body">
+                <h3>Organizational Membership</h3>
+                <p>
+                  Join as a lab, company, institute, or project. Organizational membership
+                  does not require GitHub.
+                </p>
+                <button
+                  className={`button ${styles.ncorButton}`}
+                  type="button"
+                  onClick={() => goToApplication('organization')}
+                >
+                  Apply as an Organization
+                </button>
+              </div>
+            </div>
+          </div>
   </div>
 </div>
             <BenefitsSection />
